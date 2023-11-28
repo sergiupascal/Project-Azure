9ccc0788c8210b791e01f7adb92a02a4b8c50edd
Sergiu Pascal <segisinc@gmail.com>
2023-11-28 11:07:24 -0600

    ss codes replace

--- ss.tf
+++ ss.tf
@@ -1,118 +1,101 @@
 resource "azurerm_public_ip" "project-public-ip" {
-  name                = "public-ip"
+  name                = "publicip"
   location            = azurerm_resource_group.projectazure.location
   resource_group_name = azurerm_resource_group.projectazure.name
   allocation_method   = "Static"
-  domain_name_label   = azurerm_resource_group.projectazure.name
-
-  tags = {}
 }
 
 resource "azurerm_lb" "project-lb" {
-  name                = "project-lb"
+  name                = "lb"
   location            = azurerm_resource_group.projectazure.location
   resource_group_name = azurerm_resource_group.projectazure.name
 
   frontend_ip_configuration {
-    name                 = "PublicIPAddress"
+    name                 = "internal"
     public_ip_address_id = azurerm_public_ip.project-public-ip.id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "bpepool" {
-  loadbalancer_id = azurerm_lb.project-lb.id
-  name            = "BackEndAddressPool"
+  name                = "backend-pool"
+  resource_group_name = azurerm_resource_group.projectazure.name
+  loadbalancer_id     = azurerm_lb.project-lb.id
 }
 
 resource "azurerm_lb_nat_pool" "lbnatpool" {
+  name                           = "nat-pool"
   resource_group_name            = azurerm_resource_group.projectazure.name
-  name                           = "ssh"
   loadbalancer_id                = azurerm_lb.project-lb.id
+  frontend_ip_configuration_name = "internal"
   protocol                       = "Tcp"
-  frontend_port_start            = 50000
-  frontend_port_end              = 50119
-  backend_port                   = 22
-  frontend_ip_configuration_name = "PublicIPAddress"
+  frontend_port_start            = 80
+  frontend_port_end              = 90
+  backend_port                   = 8080
 }
 
 resource "azurerm_lb_probe" "lbprobe" {
-  loadbalancer_id = azurerm_lb.project-lb.id
-  name            = "http-probe"
-  protocol        = "Http"
-  request_path    = "/health"
-  port            = 8080
+  name                = "lb-probe"
+  resource_group_name = azurerm_resource_group.projectazure.name
+  loadbalancer_id     = azurerm_lb.project-lb.id
+  port                = 22
+  protocol            = "Tcp"
 }
 
-resource "azurerm_virtual_machine_scale_set" "project-ss" {
-  name                = "project-scaleset"
-  location            = azurerm_resource_group.projectazure.location
-  resource_group_name = azurerm_resource_group.projectazure.name
+resource "azurerm_lb_rule" "lbrule" {
+  name                           = "lb-rule"
+  resource_group_name            = azurerm_resource_group.projectazure.name
+  loadbalancer_id                = azurerm_lb.project-lb.id
+  probe_id                       = azurerm_lb_probe.lbprobe.id
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+  frontend_ip_configuration_name = "internal"
+  protocol                       = "Tcp"
+  frontend_port                  = 22
+  backend_port                   = 22
+}
 
-  # automatic rolling upgrade
-  automatic_os_upgrade = true
-  upgrade_policy_mode  = "Rolling"
+resource "azurerm_linux_virtual_machine_scale_set" "project-vmss" {
+  name                            = "vmss"
+  resource_group_name             = azurerm_resource_group.projectazure.name
+  location                        = azurerm_resource_group.projectazure.location
+  sku                             = "Standard_F2"
+  instances                       = 2
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
+  health_probe_id                 = azurerm_lb_probe.lbprobe.id
+  upgrade_mode                    = "Rolling"
 
-  rolling_upgrade_policy {
-    max_batch_instance_percent              = 20
-    max_unhealthy_instance_percent          = 20
-    max_unhealthy_upgraded_instance_percent = 5
-    pause_time_between_batches              = "PT0S"
-  }
-
-  # required when using rolling upgrade policy
-  health_probe_id = azurerm_lb_probe.lbprobe.id
-
-  sku {
-    name     = "Standard_F2"
-    tier     = "Standard"
-    capacity = 2
-  }
-
-  storage_profile_image_reference {
+  source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
 
-  storage_profile_os_disk {
-    name              = ""
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
   }
 
-  storage_profile_data_disk {
-    lun           = 0
-    caching       = "ReadWrite"
-    create_option = "Empty"
-    disk_size_gb  = 10
-  }
-
-  os_profile {
-    computer_name_prefix = "testvm"
-    admin_username       = "myadmin"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-
-    ssh_keys {
-      path     = "/home/myadmin/.ssh/authorized_keys"
-      key_data = file("~/.ssh/id_rsa.pub")
-    }
-  }
-
-  network_profile {
-    name    = "terraformnetworkprofile"
+  network_interface {
+    name    = "example"
     primary = true
 
     ip_configuration {
-      name                                   = "IPConfiguration"
+      name                                   = "internal"
       primary                                = true
       subnet_id                              = azurerm_subnet.subnet1.id
       load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
       load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.lbnatpool.id]
     }
   }
+
+  rolling_upgrade_policy {
+    max_batch_instance_percent              = 21
+    max_unhealthy_instance_percent          = 22
+    max_unhealthy_upgraded_instance_percent = 23
+    pause_time_between_batches              = "PT30S"
+  }
+
+  depends_on = [azurerm_lb_rule.lbrule]
 }
