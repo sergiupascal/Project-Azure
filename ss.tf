c6c2cfd202827ed95958ad15deccb9869ddcbf24
Sergiu Pascal <segisinc@gmail.com>
2023-11-28 10:24:10 -0600

    replaced ss codes

--- ss.tf
+++ ss.tf
@@ -43,38 +43,76 @@ resource "azurerm_lb_probe" "lbprobe" {
   port            = 8080
 }
 
-resource "azurerm_linux_virtual_machine_scale_set" "project-vmss" {
-  name                = "project-vmss"
-  resource_group_name = azurerm_resource_group.projectazure.name
+resource "azurerm_virtual_machine_scale_set" "project-ss" {
+  name                = "project-scaleset"
   location            = azurerm_resource_group.projectazure.location
-  sku                 = "Standard_F2"
-  instances           = 2
-  admin_username      = "adminuser"
-  admin_password      = "pa$$w0rd"
-  custom_data         = filebase64("userdata.sh")
-  health_probe_id                 = azurerm_lb_probe.lbprobe.id
-  disable_password_authentication = false
+  resource_group_name = azurerm_resource_group.projectazure.name
 
-  source_image_reference {
-    publisher = "OpenLogic"
-    offer     = "CentOS"
-    sku       = "7_9"
+  # automatic rolling upgrade
+  automatic_os_upgrade = true
+  upgrade_policy_mode  = "Rolling"
+
+  rolling_upgrade_policy {
+    max_batch_instance_percent              = 20
+    max_unhealthy_instance_percent          = 20
+    max_unhealthy_upgraded_instance_percent = 5
+    pause_time_between_batches              = "PT0S"
+  }
+
+  # required when using rolling upgrade policy
+  health_probe_id = azurerm_lb_probe.lbprobe.id
+
+  sku {
+    name     = "Standard_F2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
-  network_interface {
-    name    = "example"
+
+  storage_profile_data_disk {
+    lun           = 0
+    caching       = "ReadWrite"
+    create_option = "Empty"
+    disk_size_gb  = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "testvm"
+    admin_username       = "myadmin"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/myadmin/.ssh/authorized_keys"
+      key_data = file("~/.ssh/id_rsa.pub")
+    }
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
     primary = true
-    network_security_group_id = azurerm_network_security_group.project-nsg.id
 
     ip_configuration {
-      name      = "internal3"
-      primary   = true
-      subnet_id = azurerm_subnet.subnet3.id
+      name                                   = "IPConfiguration"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet1.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+      load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.lbnatpool.id]
     }
   }
 }
