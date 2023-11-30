4575ce17917958588ae7e9f7400da0774860cc3d
Sergiu Pascal <segisinc@gmail.com>
2023-11-30 10:17:52 -0600

    Create load balancer rules

--- ss.tf
+++ ss.tf
@@ -43,15 +43,29 @@ resource "azurerm_lb_probe" "ssh" {
   port                = 22
 }
 
-resource "azurerm_lb_rule" "lbrule" {
-  name                           = "lb-rule"
+# Create Load Balancer Rule HTTP
+resource "azurerm_lb_rule" "lbrule-http" {
+  name                           = "lb-rule-http"
   loadbalancer_id                = azurerm_lb.project-lb.id
   probe_id                       = azurerm_lb_probe.http.id
-  frontend_ip_configuration_name = "internal"
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
+  frontend_ip_configuration_name = "internal"
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
+}
+
+# Create Load Balancer  Rule  SSH
+resource "azurerm_lb_rule" "ssh" {
+  name                           = "lb-rule-ssh"
+  loadbalancer_id                = azurerm_lb.project-lb.id
+  probe_id                       = azurerm_lb_probe.ssh.id
+  protocol                       = "Tcp"
+  frontend_port                  = 22
+  backend_port                   = 22
+  frontend_ip_configuration_name = "internal"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
+  
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "project-vmss" {
