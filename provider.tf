9c8b24ed61a580419cd7d7aa32bfe137c4baa67b
Sergiu Pascal <segisinc@gmail.com>
2023-11-30 10:05:09 -0600

    added aws provider and route53

--- provider.tf
+++ provider.tf
@@ -4,4 +4,18 @@ provider "azurerm" {
   features {}
 }
 
+# Configure the AWS account
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
 
+# AWS Route 53
+resource "aws_route53_record" "projectwordpressdb" {
+  zone_id = ""
+  name    = "wordpress.net"
+  type    = "A"
+  ttl     = 300
+  records = [azurerm_public_ip.project-public-ip.ip_address]
+}
