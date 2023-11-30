77542c44e2069f2638ad31cb9ba81e5e44b4ef47
Sergiu Pascal <segisinc@gmail.com>
2023-11-30 09:43:35 -0600

    edited mysql file

--- mysql.tf
+++ mysql.tf
@@ -5,7 +5,7 @@ resource "random_string" "random" {
   upper            = false
 }
 
-
+# MYSQL Server
 resource "azurerm_mysql_server" "projectwordpress" {
   name                = "projectwordpress-${random_string.random.result}"
   location            = azurerm_resource_group.projectazure.location
@@ -23,14 +23,33 @@ resource "azurerm_mysql_server" "projectwordpress" {
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  ssl_enforcement_enabled           = false
+  ssl_minimal_tls_version_enforced  = "TLSEnforcementDisabled"
 }
 
+# MYSQL Database
 resource "azurerm_mysql_database" "project-db-wordpress" {
   name                = "project-db"
   resource_group_name = azurerm_resource_group.projectazure.name
   server_name         = azurerm_mysql_server.projectwordpress.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
+}
+
+# MySQL Firewall - Access to AWS Route 53
+resource "azurerm_mysql_firewall_rule" "azure" {
+  name                = "public-internet"
+  resource_group_name = azurerm_resource_group.projectazure.name
+  server_name         = azurerm_mysql_server.projectwordpress.name
+  start_ip_address    = azurerm_public_ip.project-public-ip.ip_address
+  end_ip_address      = azurerm_public_ip.project-public-ip.ip_address
+}
+
+# MySQL Firewall - Access to Azure Resources
+resource "azurerm_mysql_firewall_rule" "wordpress" {
+  name                = "mysql-firewall-rule"
+  resource_group_name = azurerm_resource_group.projectazure.name
+  server_name         = azurerm_mysql_server.projectwordpress.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 }
