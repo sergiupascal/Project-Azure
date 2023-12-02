3c4252f9ccc4b2cc8acd79f9b82769c5f20f9dcd
Sergiu Pascal <segisinc@gmail.com>
2023-12-02 14:08:39 -0600

    update test/provider file

--- test/provider.tf
+++ test/provider.tf
@@ -4,18 +4,3 @@ provider "azurerm" {
   features {}
 }
 
-# Configure the AWS account
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.accesskey
-  secret_key = var.secretkey
-}
-
-# AWS Route 53
-resource "aws_route53_record" "projectwordpressdb" {
-  zone_id = var.zoneid
-  name    = "wordpress-${var.location}"
-  type    = "A"
-  ttl     = 300
-  records = [azurerm_public_ip.project-public-ip.ip_address]
-}
